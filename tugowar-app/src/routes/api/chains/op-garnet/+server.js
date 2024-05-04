2479ccb63ff23a8d00b9b4dda47650a8131b5505
Robin Bryce <robin.bryce@datatrails.ai>
2024-05-04 15:15:10 +0100

    feat: metamask integration

--- tugowar-app/src/routes/api/chains/op-garnet/+server.js
+++ tugowar-app/src/routes/api/chains/op-garnet/+server.js
@@ -13,15 +13,20 @@ export function GET() {
     chainId: Number(env['PUBLIC_OP_GARNET_CHAINID']),
     polling: env['PUBLIC_OP_GARNET_POLLING'] ?? 2000,
     arenaProxy: env['PUBLIC_OP_GARNET_TUGAWAR_ADDRESS'],
-    arenaDeployer: env['PUBLIC_OP_GARNET_TUGAWAR_DEPLOYER']
+    arenaDeployer: env['PUBLIC_OP_GARNET_TUGAWAR_DEPLOYER'],
+
   };
   if (secrets['OP_GARNET_URL'])
     chain.url = secrets['OP_GARNET_URL'];
   if (!chain.url && env['PUBLIC_OP_GARNET_URL'])
     chain.url = env['PUBLIC_OP_GARNET_URL'];
+  if (chain.url)
+    chain.chainConfig.rpcTarget = chain.url;
   if (env['PUBLIC_OP_GARNET_BRIDGE'])
     chain.bridge = env['PUBLIC_OP_GARNET_BRIDGE'];
-  if (env['PUBLIC_OP_GARNET_ETHERSCAN_URL'])
+  if (env['PUBLIC_OP_GARNET_ETHERSCAN_URL']) {
     chain.etherscanUrl = env['PUBLIC_OP_GARNET_ETHERSCAN_URL'];
+    chain.chainConfig.blockExplorerUrl = chain.etherscanUrl;
+  }
   return json(chain);
 }
